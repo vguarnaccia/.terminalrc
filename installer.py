301335b65e23f95338d772e1b06626872b282293
Vincent Guarnaccia <Vincent.Guarnaccia@gmail.com>
2017-05-13 23:37:00 -0700

    add remove option to installer and installer cleanup

--- installer.py
+++ installer.py
@@ -1,44 +1,89 @@
 #! /usr/bin/env python
-"""TODO:
-    Make a real docstring with examples
-    Add a remove option
-    Test to see which versions of python this works on
+# -*- coding: utf-8 -*-
+"""A simple script for installing the terminal configuration files into your bashrc or config.fish.
+
+This script is meant to be used as a command line interface tool. It can also remove the script if
+you no longer want it. You can find terminalrc at https://github.com/vguarnaccia/terminalrc
+
+Examples:
+    $ python installer.py
+    Install Successful!
+
+    $ python installer.py
+    Already installed :)
+
+    $ python installer.py --remove bashrc
+    Script removed. Remember to delete ~/.terminalrc
+
+TODO:
+    * Implement for config.fish.
 """
 
 import argparse
+from collections import namedtuple
 import os
 import re
 
-def get_parser():
-    """Create parser object for stdin"""
-    parser = argparse.ArgumentParser(
-        description=
-        'Simple CLI tool for installing "terminalrc" for bash and fish'
-    )
+def parse_args(params=None):
+    """Create Namespace object from a string or argv[1:].
+
+    Arg:
+        params (str): a string in the form of command arguments.
+            If none provided, it will pull from argv.
+    """
+    parser = argparse.ArgumentParser(description=
+                                     'Simple CLI tool to install "terminalrc" for bash and fish')
     parser.add_argument(
-        '-f',
+        '-b',
         '--bashrc',
-        help='Specify the file path to your .bashrc',
+        help='Specify the file path to your .bashrc if it\'s not ~/.bashrc',
         type=argparse.FileType('r+'),
         default=os.path.expanduser('~') + os.sep + '.bashrc'
     )
-    return parser
+    parser.add_argument('--remove', choices=['bashrc', 'config.fish'],
+                        help='remove script from bashrc or config.fish')
+    default_fish = os.path.join(os.path.expanduser('~'),
+                                '.config', 'fish', 'config.fish')
+    parser.add_argument(
+        '-f',
+        '--fish',
+        help=('Specify the file path to your config.fish. '
+              'It\'s probably %s' % default_fish),
+        type=argparse.FileType('r+'),
+    )
+    return parser.parse_args(params.split() if params else None)
 
 
-def main(command=None):
-    """Add or remove the bash script """
+# A Script needs text and a regex that matches the text
+Script = namedtuple('Script', ['text', 'regex'])
+
+
+def main(params=None):
+    """Add or remove the bash script
+
+    TODO:
+        Extend for config.fish
+    """
     # Get args from main or from console
-    args = get_parser().parse_args(command.split() if command else None)
+    args = parse_args(params)
 
-    # Format script for bashrc
-    script = '\nsource ~/.terminalrc/bash.config'
-    regex = re.compile(r'\nsource ~/\.terminalrc/bash\.config')
+    # Script for bashrc
+    bash_script = Script('\nsource ~/.terminalrc/bash.config',
+                         re.compile(r'\nsource ~/\.terminalrc/bash\.config'))
     with args.bashrc as bashrc:
         content = bashrc.read()
-        if regex.search(content):
+        if args.remove == 'bashrc':
+
+            # Rewrite bashrc without script
+            uninstalled = bash_script.regex.sub('', content)
+            bashrc.seek(0)
+            bashrc.write(uninstalled)
+            bashrc.truncate()
+            print('Script removed. Remember to delete ~/.terminalrc')
+        elif bash_script.regex.search(content):
             print('Already installed :)')
         else:
-            bashrc.write(script)
+            bashrc.write(bash_script.text)
             print('Install successful!')
 
 
